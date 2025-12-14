ac28623bcfb5e79f82f18ed262fb130139a58b93
Neal Guarddin <guarddin29@gmail.com>
2025-12-14 15:44:02 +0700

    fix static files

--- ppm_afm_web/settings.py
+++ ppm_afm_web/settings.py
@@ -128,7 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images) - Diurus oleh WhiteNoise
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Tambahkan baris ini jika folder static kamu ada di root project
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Media files (Uploads) - Diurus oleh Cloudinary
 MEDIA_URL = '/media/'
